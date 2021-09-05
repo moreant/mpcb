75379e26daa4d589396d7f7b21b00e09eda06c4b
moreant <moreant@foxmail.com>
2021-09-05 15:29:25 +0800

    修改文本错误

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -17,7 +17,7 @@ import ExtraLink from './ExtraLink.vue'
   >
     <div class="text-6xl pb-4 font-bold">Hi, 欢迎使用 MPCB！</div>
     <div class="flex flex-row w-full justify-between">
-      <div class="text-3xl">简高效的批量下载魅族云相册中的图片。</div>
+      <div class="text-3xl">简洁高效的批量下载魅族云相册中的图片。</div>
 
       <a href="https://github.com/moreant/mpcb" target="_blank" class="btn-link text-3xl text-white"
         >使用文档 <extra-link class="h-6 inline"
